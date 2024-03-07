43e8fb8275da2a8e1f49594f73e11cd4038aa2fd
vep002 <vep002@gmail.com>
2024-03-07 13:29:04 -0600

    basic components added

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as NavBar } from './NavBar/NavBar';
+export { default as Landing } from './Landing/Landing';
+export { default as AboutMe } from './AboutMe/AboutMe';
+export { default as Education } from './Education/Education';
+export { default as Skills } from './Skills/Skills';
+export { default as Experience } from './Experience/Experience';
+export { default as Projects } from './Projects/Projects';
+export { default as Contact } from './Contact/Contact';
+export { default as Footer } from './Footer/Footer';
