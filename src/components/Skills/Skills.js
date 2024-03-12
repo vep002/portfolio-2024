b79a5bbc5630a2f9ae9ea23f95ac83749c434751
vep002 <vep002@gmail.com>
2024-03-12 15:01:35 -0500

    added content to skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
+
+import { skillsData } from "../../data/skillsData";
 
 function Skills() {
   return (
-    <div>
-      <h1>Skills</h1>
+    <div className="skills">
+      <div className="skillsHeader">
+        <h2>Skills</h2>
+      </div>
+      <div className="skill--scroll">
+        <Marquee
+          gradient={false}
+          speed={80}
+          pauseOnHover={true}
+          pauseOnClick={true}
+          delay={0}
+          play={true}
+          direction="left"
+          >
+            {skillsData.map((skill, id) => (
+              <div className="skill-box" key={id}>
+                <h3>{skill}</h3>
+              </div>
+            ))}
+          </Marquee>
+      </div>
     </div>
   );
 }
