21c99862176c3e3b383b67e6e851f7125a15e6a7
vep002 <vep002@gmail.com>
2024-03-15 11:57:54 -0500

    added content to footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { headerData } from "../../data/headerData";
+
 
 function Footer() {
+
+  const shortName = (name) => {
+    if(name.length > 10){
+      return name.split(" ")[0];
+    } else {
+      return name
+    }
+  }
+
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer">
+      <p>
+        Made with 
+        <span role="img" aria-label="love">
+          ❤️
+        </span>
+          by {shortName(headerData.name)}
+      </p>
     </div>
   );
 }
